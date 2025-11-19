da3184048ed8c83067e0c247a8fba331a7efbeef
Mahad Kalam <mahadkalam1234@gmail.com>
2025-11-19 21:32:35 +0000

    feat: basic docs

--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -23,7 +23,7 @@ export const Layout = ({
           href="https://fonts.googleapis.com/css2?family=Google+Sans:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
