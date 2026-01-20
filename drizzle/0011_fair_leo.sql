e00e69534d2f01ca9bb9b424ec27564024ec512b
Mahad Kalam <mahadkalam1234@gmail.com>
2026-01-20 19:57:38 +0000

    update, part 2

--- drizzle/0011_fair_leo.sql
+++ drizzle/0011_fair_leo.sql
@@ -1,3 +1,3 @@
 DROP INDEX "request_logs_user_timestamp_idx";--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "spending_limit_usd" numeric(10, 8) DEFAULT '10';--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "spending_limit_usd" numeric(10, 8) DEFAULT '8';--> statement-breakpoint
 CREATE INDEX "request_logs_user_timestamp_cost_idx" ON "request_logs" USING btree ("user_id","timestamp" DESC NULLS LAST,"cost");
